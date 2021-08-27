839678b8fcc8640642cd1d6c5fdbf39ca3c03aa4
chrisshum <60720132+chrisshum@users.noreply.github.com>
2021-08-27 15:11:55 -0700

    Add files via upload

---
+++ GUI for pc and serial code for USB protocol/WinFormSerialExample/WindowsSerialCommander/MainForm.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace WindowsSerialCommander
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.com6 = new System.IO.Ports.SerialPort(this.components);
+            this.btnLEDOn = new System.Windows.Forms.Button();
+            this.btnLEDOff = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // com6
+            // 
+            this.com6.PortName = "COM6";
+            // 
+            // btnLEDOn
+            // 
+            this.btnLEDOn.Location = new System.Drawing.Point(6, 19);
+            this.btnLEDOn.Name = "btnLEDOn";
+            this.btnLEDOn.Size = new System.Drawing.Size(124, 78);
+            this.btnLEDOn.TabIndex = 0;
+            this.btnLEDOn.Text = "Flash VEP";
+            this.btnLEDOn.UseVisualStyleBackColor = true;
+            this.btnLEDOn.Click += new System.EventHandler(this.btnLEDOn_Click);
+            // 
+            // btnLEDOff
+            // 
+            this.btnLEDOff.Location = new System.Drawing.Point(6, 115);
+            this.btnLEDOff.Name = "btnLEDOff";
+            this.btnLEDOff.Size = new System.Drawing.Size(124, 78);
+            this.btnLEDOff.TabIndex = 1;
+            this.btnLEDOff.Text = "Pattern VEP";
+            this.btnLEDOff.UseVisualStyleBackColor = true;
+            this.btnLEDOff.Click += new System.EventHandler(this.btnLEDOff_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 55);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "VOL UP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(304, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 55);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "VOL DN";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 106);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "BAEP";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 132);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 61);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "left ear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(96, 132);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 60);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "right ear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(136, 19);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(55, 174);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Left Visual";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(197, 19);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(55, 174);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Right Visual";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(185, 36);
+            this.trackBar1.Maximum = 99;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 156);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLEDOn);
+            this.groupBox1.Controls.Add(this.btnLEDOff);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Location = new System.Drawing.Point(12, 217);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(272, 199);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Visual";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.trackBar1);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(237, 199);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sound";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Volume";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button9);
+            this.groupBox3.Controls.Add(this.button8);
+            this.groupBox3.Location = new System.Drawing.Point(269, 22);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(210, 116);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "SSEP";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(6, 19);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(90, 68);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "Left Shock";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click_1);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(110, 19);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(90, 68);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "Right Shock";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 454);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "Windows Serial Commander";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort com6;
+        private System.Windows.Forms.Button btnLEDOn;
+        private System.Windows.Forms.Button btnLEDOff;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ GUI for pc and serial code for USB protocol/WinFormSerialExample/WindowsSerialCommander/MainForm.cs
@@ -0,0 +1,158 @@
+﻿//=======================================================================================
+//
+//  Purpose:    Send simple commands down the serial line to device waiting for commands.
+//
+//  Copyright (C) 2011 Mark Stevens
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software") to use
+//  the Software in a non-commercial environment subject to the following conditions:
+//
+//  1.  The above copyright notice and this permission notice shall be included in
+//      all copies or substantial portions of the Software.
+//  2.  The original author shall be acknowledged in any software and documentation
+//      including (but not restricted to) books, articles and online posts.
+//
+//  Use in a commercial environment is subject to the written consent of the author
+//  and/or copyright holder.
+//
+//  The Software, documentation, instructions for use and associated materials
+//  are provided "as is" without warranty of any kind express or implied.  The
+//  author and copyright holders shall in no way be held liable for any claim
+//  or damages arising as a result of the use of the software and/or documentation.
+//
+//=======================================================================================
+using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace WindowsSerialCommander
+{
+    public partial class MainForm : Form
+    {
+        #region Constructor(s)
+
+        /// <summary>
+        /// Constructor for the application.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Initialise the application.
+        /// </summary>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            com6 = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);
+            com6.Open();
+        }
+
+        /// <summary>
+        /// Send a command to the device listening on the com port.
+        /// </summary>
+        private void btnLEDOn_Click(object sender, EventArgs e)
+        {
+            com6.Write("FLED\r");
+        }
+
+        /// <summary>
+        /// Send a command to turn the LED off.
+        /// </summary>
+        private void btnLEDOff_Click(object sender, EventArgs e)
+        {
+            com6.Write("PLED\r");
+        }
+
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            com6.Write("Volume+\r");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            com6.Write("BAEP\r");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            com6.Write("Volume-\r");
+        }
+
+       
+        
+
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            com6.Write("Leftear\r");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            com6.Write("Rightear\r");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            com6.Write("Both\r");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            com6.Write("Leftvisual\r");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            com6.Write("Rightvisual\r");
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int vol = trackBar1.Value;
+            vol.ToString();
+            if (vol < 10)
+            {
+                com6.Write("vol0" + vol + "\r");
+
+            }
+            else
+            {
+                com6.Write("vol" + vol + "\r");
+            }
+        }
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            com6.Write("leftShock\r");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            com6.Write("rightShock\r");
+        }
+
+
+      
+
+        
+        
+        
+        
+        
+      
+
+     
+
+       
+    }
+}
